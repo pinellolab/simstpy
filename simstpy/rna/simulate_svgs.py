3cde053126a74874b4c7cffc2a4969644ca7f3ef
lzj1769 <lzj1769@gmail.comz>
2023-03-23 19:16:31 -0400

    using cholesky for sampling

--- simstpy/rna/simulate_svgs.py
+++ simstpy/rna/simulate_svgs.py
@@ -146,7 +146,9 @@ def svgs_rand_covariance(
     for i in range(n_svgs):
         cov = make_spd_matrix(n_dim=n_locations, random_state=i)
         cov = np.multiply(cov, sigma)
-        exp[:, i] = rng.multivariate_normal(mean=np.zeros(n_locations), cov=cov)
+        exp[:, i] = rng.multivariate_normal(
+            mean=np.zeros(n_locations), cov=cov, method="cholesky"
+        )
 
     return exp
 
@@ -177,7 +179,9 @@ def svgs_rbf_kernel(
         cov = kernel(coords)
         cov = check_pos_semidefinite(cov=cov)
         cov = np.multiply(cov, sigma)
-        exp[:, i] = rng.multivariate_normal(mean=np.zeros(n_locations), cov=cov)
+        exp[:, i] = rng.multivariate_normal(
+            mean=np.zeros(n_locations), cov=cov, method="cholesky"
+        )
 
     return exp
 
@@ -221,6 +225,8 @@ def svgs_period_kernel(
         cov = kernel(coords)
         cov = check_pos_semidefinite(cov=cov)
         cov = np.multiply(cov, sigma)
-        exp[:, i] = rng.multivariate_normal(mean=np.zeros(n_locations), cov=cov)
+        exp[:, i] = rng.multivariate_normal(
+            mean=np.zeros(n_locations), cov=cov, method="cholesky"
+        )
 
     return exp
